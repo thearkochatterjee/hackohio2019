5b0392b74993e54ff7c686a04fbe8ca9ac3996ab
Michael <57294690+mfong121@users.noreply.github.com>
2019-11-02 13:33:38 -0400

    Initialized visual components in activity_main to MainActivity java class
    Created onClickListener for loginButton

--- door opener v2/app/src/main/java/com/example/dooropenerv2/MainActivity.java
+++ door opener v2/app/src/main/java/com/example/dooropenerv2/MainActivity.java
@@ -5,9 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+//login to account, account has access to different rooms with different IDs
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.door_toggle);
+
+            EditText usernameField = (EditText)findViewById(R.id.usernameField);
+            EditText passwordField = (EditText)findViewById(R.id.passwordField);
+            Button loginButton = (Button)findViewById(R.id.loginButton);
+
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Code here executes on button click
+
+                }
+            });
+        }
 }
--- door opener v2/app/src/main/java/com/example/dooropenerv2/doorToggle.java
+++ door opener v2/app/src/main/java/com/example/dooropenerv2/doorToggle.java
@@ -24,3 +24,4 @@ public class doorToggle extends AppCompatActivity {
         });
     }
 }
+
